404419960ce7f0c27faed31a40eca7976a0a6444
maikhol <51258901+maikhol@users.noreply.github.com>
2022-05-03 17:04:57 -0700

    Add files via upload
    All the necessary files to start and run the program.

---
+++ Unit1.h
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+#include <FMX.ImgList.hpp>
+#include <System.ImageList.hpp>
+#include <FMX.Objects.hpp>
+#include <FMX.Styles.Objects.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+	TButton *Button2;
+	TButton *Button3;
+	TButton *Button4;
+	TButton *Button5;
+	TButton *Button6;
+	TButton *Button7;
+	TButton *Button8;
+	TButton *Button9;
+	TLabel *Label1;
+	TLabel *currentPlayerSymbolLabel;
+	TLabel *Label2;
+	TLabel *winnerLabel;
+	TImageList *ImageList1;
+	TGlyph *Glyph1;
+	void __fastcall ButtonClick(TObject *Sender);
+
+private:	// User declarations
+	bool TForm1:: checkWinner();
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
